60f271fe10d9df1a1b129b130293ad3ae8e2879c
jinhongjian <654457286@qq.com>
2020-07-14 22:57:26 +0800

    20200714

---
+++ src/main/java/a_26_sub_tree/BinaryTreeNode.java
@@ -0,0 +1,15 @@
+package a_26_sub_tree;
+
+public class BinaryTreeNode {
+    public int value;
+    public BinaryTreeNode pLeft;
+    public BinaryTreeNode pRight;
+
+    public BinaryTreeNode(int value) {
+        this.value = value;
+    }
+
+    public BinaryTreeNode() {
+
+    }
+}
---
+++ src/main/java/a_32_print_binarytree_by_level/BinaryTreeNode.java
@@ -0,0 +1,15 @@
+package a_32_print_binarytree_by_level;
+
+public class BinaryTreeNode {
+    public int value;
+    public BinaryTreeNode pLeft;
+    public BinaryTreeNode pRight;
+
+    public BinaryTreeNode(int value) {
+        this.value = value;
+    }
+
+    public BinaryTreeNode() {
+
+    }
+}
