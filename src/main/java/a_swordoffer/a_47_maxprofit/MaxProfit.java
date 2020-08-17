bc3449b6096b90a48dd4470c9f07586472d00467
jinhongjian <654457286@qq.com>
2020-08-18 01:27:22 +0800

    礼物的最大价值

---
+++ src/main/java/a_swordoffer/a_47_maxprofit/MaxProfit.java
@@ -0,0 +1,27 @@
+package a_swordoffer.a_47_maxprofit;
+
+/**
+ * 第47题，动态规划，礼物的最大价值
+ */
+public class MaxProfit {
+    public static int maxProfit(int[][] nums, int rows, int cols) {
+        int[][] dp = new int[rows + 1][cols + 1]; //哨兵，避免边界判断
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= cols; j++) {
+                dp[i][j] = nums[i - 1][j - 1] + Math.max(dp[i][j - 1], dp[i - 1][j]);
+            }
+        }
+        return dp[rows][cols];
+    }
+
+    public static void main(String[] args) {
+        int[][] nums = new int[][]{
+                {1, 10, 3, 8},
+                {12, 2, 9, 6},
+                {5, 7, 4, 11},
+                {3, 7, 16, 5}
+        };
+
+        System.out.println(maxProfit(nums,4,4));
+    }
+}
